265c61737033fd0c1a539b4fe2026d0bbee835b4
Tiago Angelo <kurtis.angelo@gmail.com>
2021-12-19 11:23:02 -0300

    Infers service name if it has not been defined

--- tracer.go
+++ tracer.go
@@ -1,32 +1,49 @@
 package tracing
 
 import (
-	"fmt"
 	"io"
+	"runtime"
+	"strings"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go/config"
 )
 
 // Initialize create an instance of Jaeger Tracer and sets it as GlobalTracer.
-func Initialize(module string, options ...TracerOptionFunc) (io.Closer, error) {
-	set(module, module)
+func Initialize(options ...TracerOptionFunc) (io.Closer, error) {
 	cfg := new(config.Configuration)
 
 	for _, fn := range options {
 		if fn == nil {
 			continue
 		}
-		fn(cfg)
+		if err := fn(cfg); err != nil {
+			return nil, err
+		}
 	}
 
 	cfg, err := cfg.FromEnv()
 	if err != nil {
-		return nil, fmt.Errorf("cannot read environment variables: %w", err)
+		return nil, err
 	}
+
+	// if the service name has not been defined, infer from the caller's package name
+	if cfg.ServiceName == "" {
+		pc, _, _, _ := runtime.Caller(1)
+		funcName := runtime.FuncForPC(pc).Name()
+		lastSlash := strings.LastIndexByte(funcName, '/')
+		if lastSlash < 0 {
+			lastSlash = 0
+		} else {
+			lastSlash++
+		}
+		firstDot := strings.IndexByte(funcName[lastSlash:], '.') + lastSlash
+		cfg.ServiceName = funcName[lastSlash:firstDot]
+	}
+
 	tracer, closer, err := cfg.NewTracer()
 	if err != nil {
-		return nil, fmt.Errorf("cannot create tracer: %w", err)
+		return nil, err
 	}
 
 	opentracing.SetGlobalTracer(tracer)
